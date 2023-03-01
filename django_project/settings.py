4f14d8162165447ad6970055e7450a5f17b9f3a2
usanaphtal112 <usanaphtal112@gmail.com>
2023-03-01 21:22:03 +0200

    Added bootstrap, Password resert and Password change

--- django_project/settings.py
+++ django_project/settings.py
@@ -37,7 +37,12 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # 3rd Party
+    "crispy_forms",  # new
+    "crispy_bootstrap5",  # new
+    # Local apps
     "accounts.apps.AccountsConfig",
+    "pages.apps.PagesConfig",
 ]
 
 MIDDLEWARE = [
@@ -127,3 +132,8 @@ AUTH_USER_MODEL = "accounts.CustomUser"  # new
 
 LOGIN_REDIRECT_URL = "home"  # new
 LOGOUT_REDIRECT_URL = "home"  # new
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"  # new
+CRISPY_TEMPLATE_PACK = "bootstrap5"  # new
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"  # new
--- django_project/urls.py
+++ django_project/urls.py
@@ -15,11 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("accounts.urls")),  # new
     path("accounts/", include("django.contrib.auth.urls")),  # new
-    path("", TemplateView.as_view(template_name="home.html"), name="home"),  # new
+    path("", include("pages.urls")),  # new
 ]
